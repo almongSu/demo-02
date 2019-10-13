d18375363d9705e66563662c7391c83ddb85d1cf
almondSu <52618216+almongSu@users.noreply.github.com>
2019-10-13 22:50:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -22,19 +22,24 @@ var server = http.createServer(function(request, response){
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
   if(path === '/'){
-    response.statusCode = 200
+    response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`二哈`)
     response.end()
   } else if(path === '/x'){
-    response.statusCode = 200
+    response.statusCode = 200;
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
+    response.end()
+  } else if(path === '/y.css'){
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.write(`console.log('这是JS内容')`)
     response.end()
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
+    response.write(`[你访问的页面不存在]`)
     response.end()
   }
 
@@ -43,4 +48,5 @@ var server = http.createServer(function(request, response){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
 
